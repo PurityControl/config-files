77994f2be182553f06182099d1c9891e97284460
Craig Ferry <PurityControl@users.noreply.github.com>
2016-03-04 13:22:14 +0000

    Move evil mode with :ensure into org config.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -25,9 +25,11 @@
  ((eq system-type 'darwin)
   (set-face-attribute 'default nil :height 160)))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;; PACKAGE INSTALLATION
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Set Package repositories ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 
 ;; add melpa stable to package repos
 (require 'package)
@@ -39,35 +41,6 @@
   ;; For important compatibility libraries like cl-lib
     (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
-
-
-;; list the packages you want to install - will be preserved in src control
-(defvar my-packages '(evil))
-
-;; install the packages  if not installed
-(dolist (p my-packages)
-  (unless (package-installed-p p)
-    (package-install p)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;; CUSTOM KEYBINDINGS
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; bind F12 to toggle evil mode
-(global-set-key [f12] 'evil-mode)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(setq dotfile-dir (file-name-directory
-                   (or (buffer-file-name) load-file-name)))
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/key-chord"))
-(require 'key-chord)
-(key-chord-mode 1)
-(evil-mode 0)
-(key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
-(define-key evil-motion-state-map ";" 'evil-ex)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
