7456fbfc6ea5a8739427aceaf4957a3bf3a1ec89
Craig Ferry <PurityControl@users.noreply.github.com>
2016-04-29 07:50:06 +0100

    make shell mode login shell

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,6 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(explicit-bash-args (quote ("--noediting" "--login" "-i")))
  '(org-hide-emphasis-markers t)
  '(tramp-default-method "ssh"))
 (custom-set-faces
